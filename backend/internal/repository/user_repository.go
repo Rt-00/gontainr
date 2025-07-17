6c39e7783f39e0a2a7d582c90b9dc562cad86e0c
Rafael Toledo <toledorafael304@gmail.com>
2025-07-17 18:43:45 -0300

    feat: implement user repository with GetByUsername method

---
+++ backend/internal/repository/user_repository.go
@@ -0,0 +1,38 @@
+package repository
+
+import (
+	"database/sql"
+
+	"github.com/Rt-00/gontainr/backend/internal/domain"
+)
+
+type UserRepository struct {
+	db *sql.DB
+}
+
+func NewUserRepository(db *sql.DB) *UserRepository {
+	return &UserRepository{db: db}
+}
+
+func (userRepo *UserRepository) GetByUsername(username string) (*domain.User, error) {
+	user := &domain.User{}
+
+	err := userRepo.db.QueryRow(
+		`
+		SELECT
+		  id,
+		  username,
+		  password
+		FROM
+		  users
+		WHERE
+		  username = $1
+		`, username,
+	).Scan(&user.ID, &user.Username, &user.Password)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return user, nil
+}
